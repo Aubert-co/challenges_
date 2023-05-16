c6ebe36afded768214df3bd8af167ba9935c2c0b
Black <53127242+Aubert-co@users.noreply.github.com>
2023-05-15 23:54:51 -0300

    Update occurencyStrings.js

--- main/occurencyStrings.js
+++ main/occurencyStrings.js
@@ -1,10 +1,11 @@
 const palavras = ["casa", "carro", "casa", "moto", "carro", "carro"];
 
 
-const m =palavras.reduce((vl,tr)=>{
+const m = (array)=>array.reduce((vl,tr)=>{
     if(vl[tr])vl[tr]++
     else vl[tr] = 1
 
     return vl
 },{})
-console.log(m)
+
+module.exports = m
