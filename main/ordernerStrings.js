7d25b81fc8ebcd823869c7b4d529cb4acb5df0b0
Black <53127242+Aubert-co@users.noreply.github.com>
2023-05-15 23:55:22 -0300

    Update ordernerStrings.js

--- main/ordernerStrings.js
+++ main/ordernerStrings.js
@@ -1,8 +1,8 @@
 const array= ["um","name","quatro","qweqweqweqwe"]
 
-array.sort((a,b)=>{
+const v = (array)=>array.sort((a,b)=>{
     if(a.length > b.length)return 0
     if(b.length > a.length)return -1
     return 1
 })
-console.log(array)
+module.exports = v
