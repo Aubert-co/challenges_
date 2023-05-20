73b882cc2a84f8d2b0a7a607098a396be54ace3d
Black <53127242+Aubert-co@users.noreply.github.com>
2023-05-19 23:49:30 -0300

    Update occurencyStrings.js

--- main/occurencyStrings.js
+++ main/occurencyStrings.js
@@ -1,11 +1,16 @@
-const palavras = ["casa", "carro", "casa", "moto", "carro", "carro"];
+const words = ["casa", "carro", "casa", "moto", "carro", "carro"];
+/**
+ * 
+ 
+In this challenge, the function takes a list of words and returns an object with the frequency count of each item.
+occurencyStrings(words)=>{ casa: 2, carro: 3, moto: 1 } 
+*/
 
-
-const m = (array)=>array.reduce((vl,tr)=>{
+const occurencyStrings = (array)=>array.reduce((vl,tr)=>{
     if(vl[tr])vl[tr]++
     else vl[tr] = 1
 
     return vl
 },{})
 
-module.exports = m
+module.exports = occurencyStrings
