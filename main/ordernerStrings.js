38dc856470b1d47bb65780628a3a1020a2d53faf
Black <53127242+Aubert-co@users.noreply.github.com>
2023-05-19 23:49:41 -0300

    Update ordernerStrings.js

--- main/ordernerStrings.js
+++ main/ordernerStrings.js
@@ -1,8 +1,14 @@
-const array= ["um","name","quatro","qweqweqweqwe"]
-
-const v = (array)=>array.sort((a,b)=>{
+const array= ["name","quatro","qweqweqweqwe","um"]
+/**
+ * 
+ * In this challenge, it takes words in an array and sorts
+ *  them from the ones with fewer letters to the ones with more letters.
+ *  example: ordernerStrings(array)=>[ 'um', 'name', 'quatro', 'qweqweqweqwe' ]
+ */
+const ordernerStrings = (array)=>array.sort((a,b)=>{
     if(a.length > b.length)return 0
     if(b.length > a.length)return -1
     return 1
 })
-module.exports = v
+
+module.exports = ordernerStrings
